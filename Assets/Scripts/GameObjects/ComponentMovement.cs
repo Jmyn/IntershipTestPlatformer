10b03d246b43170bfb71e275c21c1f15221ea06d
Jmyn <jermyntanu92@gmail.com>
2015-04-02 17:30:37 +0800

    First commit

---
+++ Assets/Scripts/GameObjects/ComponentMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComponentMovement : MonoBehaviour 
+{
+
+	//TODO movement
+
+	float m_speed = 0.1f;
+	[SerializeField]
+	protected float m_jumpHeight = 11;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			//TODO move character left
+		}
+		else if (Input.GetKey (KeyCode.D)) 
+		{
+			//TODO move character right
+		}
+		else if (Input.GetKey (KeyCode.W)) 
+		{
+			//TODO make character jump
+		}
+
+	}
+	
+
+
+
+
+
+
+}
